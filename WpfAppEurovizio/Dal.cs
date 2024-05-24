b5cd66379876f346dc30aba640b13c922255b89d
Dugovits Titusz <72067053+GhostLead@users.noreply.github.com>
2024-05-24 11:09:14 +0200

    54

---
+++ WpfAppEurovizio/Dal.cs
@@ -0,0 +1,48 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfAppEurovizio
+{
+    internal class Dal
+    {
+
+
+        private int id;
+        private int ev;
+        private string orszag;
+        private string eloado;
+        private string cim;
+        private int helyezes;
+        private int pontszam;
+
+        public int Id { get => id; set => id = value; }
+        public int Ev { get => ev; set => ev = value; }
+        public string Orszag { get => orszag; set => orszag = value; }
+        public string Eloado { get => eloado; set => eloado = value; }
+        public string Cim { get => cim; set => cim = value; }
+        public int Helyezes { get => helyezes; set => helyezes = value; }
+        public int Pontszam { get => pontszam; set => pontszam = value; }
+
+        public Dal(MySqlDataReader olvaso)
+        {
+            Id = olvaso.GetInt32(0);
+
+            Ev = olvaso.GetInt32(1);
+
+            Orszag = olvaso.GetString(2);
+
+            Eloado = olvaso.GetString(3);
+
+            Cim = olvaso.GetString(4);
+
+            Helyezes = olvaso.GetInt32(5);
+
+            Pontszam = olvaso.GetInt32(6);
+        }
+
+    }
+}
---
+++ WpfAppEurovizio/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppEurovizio
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string connectionstring = "datasource=127.0.0.1;port=3306;username=root;password=;database=eurovizio;";
+        private MySqlConnection connection;
+        List<Dal> adatok;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void dgDal_Loaded(object sender, RoutedEventArgs e)
+        {
+            adatok = new List<Dal>();
+
+            try
+            {
+                connection = new MySqlConnection(connectionstring);
+                connection.Open();
+                string queryText = "SELECT id,ev,orszag,eloado,cim,helyezes,pontszam FROM dal ORDER BY id";
+                MySqlCommand query = new MySqlCommand(queryText, connection);
+                query.CommandTimeout = 60;
+                MySqlDataReader reader = query.ExecuteReader();
+                while (reader.Read())
+                {
+                    adatok.Add(new Dal(reader));
+                }
+                reader.Close();
+                connection.Close();
+                dgDal.ItemsSource = adatok;
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
